91094391dff35cafe6f748abe4c927acb0244230
ManishThulung <manish.thulung58@gmail.com>
2024-05-15 00:48:08 +0545

    todo complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,47 @@
 use std::error::Error;
-use std::fs::File;
+use std::fs::{self, OpenOptions};
 use std::io::Write;
 
-pub struct Config {
-  task: String,
-}
+pub fn add_todo (task: String) -> Result<(), Box<dyn Error>>  {
+  // let mut file = fs::File::create("todo-list.txt")?;
+  // file.write(task.as_bytes())?;
 
-impl Config {
-  pub fn build (mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-    args.next();
+  let mut data_file = OpenOptions::new()
+  .append(true)
+  .open("todo-list.txt")?;
 
-    let task = match args.next() {
-      Some(task) => task,
-      None => return Err("something went wrong"),
-    };
+  data_file.write(task.as_bytes())?;
 
-    Ok(Config{
-      task
-    })
-  }
-}
-
-pub fn add_todo (config: Config) -> Result<(), Box<dyn Error>>  {
-  let mut file = File::create("todo-list.txt")?;
-
-  file.write(config.task.as_bytes())?;
-  
   Ok(())
 }
+
+pub fn read_todo () -> Result<(), Box<dyn Error>>  {
+  let tasks = fs::read_to_string("todo-list.txt")?;
+  for task in tasks.lines() {
+    println!("{}", task);
+  }
+  Ok(())
+}
+
+
+
+// pub struct Config {
+//   task: String,
+// }
+
+// impl Config {
+//   pub fn build (mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+//     args.next();
+
+//     let task = match args.next() {
+//       Some(task) => task,
+//       None => return Err("something went wrong"),
+//     };
+
+//     Ok(Config{
+//       task
+//     })
+//   }
+// }
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,62 @@
-use std::env;
+// use std::env;
+// use std::f32::consts::E;
+// use todo::Config;
 use std::process;
-use todo::Config;
+use std::io;
 
 fn main() {
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
-        println!("args parsing error: {}", err);
-        process::exit(1);
-    });
 
-    if let Err(e) = todo::add_todo(config) {
-        println!("error while adding todo: {}", e);
-        process::exit(1);
+    // let config = Config::build(env::args()).unwrap_or_else(|err| {
+    //     println!("args parsing error: {}", err);
+    //     process::exit(1);
+    // });
+
+    // if let Err(e) = todo::add_todo(config) {
+    //     println!("error while adding todo: {}", e);
+    //     process::exit(1);
+    // }
+
+    loop {
+        println!("enter 1 for adding todo");
+        println!("enter 2 to see all todos");
+        println!("enter 3 to exit");
+
+        let mut action = String::new();
+
+        io::stdin().read_line(&mut action).expect("failed to read a number");
+
+        let action: i8 = match action.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        match action {
+            1 => {
+                println!("add task");
+                let mut task = String::new();
+                io::stdin().read_line(&mut task).expect("failed to read a number");
+
+                if let Err(e) = todo::add_todo(task) {
+                    println!("error while adding todo: {}", e);
+                    process::exit(1);
+                }
+            }
+            2 => {
+                println!("listed tasks");
+                if let Err(e) = todo::read_todo() {
+                    println!("error while adding todo: {}", e);
+                    process::exit(1);
+                }
+                break;
+            }
+            3 => {
+                println!("EXIT");
+                break;
+            }
+            _ => {
+                println!("WRONG COMMAND");
+                continue;
+            }
+        }
     }
 }
